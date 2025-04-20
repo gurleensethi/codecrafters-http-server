2859fd1b97b46acedc5eab714fcb1709e21cb69e
Gurleen Sethi <sarusethi@rocketmail.com>
2025-04-20 17:18:31 -0300

    feat: check for url path

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -22,17 +23,44 @@ func main() {
 		os.Exit(1)
 	}
 
-	n, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	n, err := conn.Read(buffer)
 	if err != nil {
-		fmt.Println("error writing data:", err.Error())
+		fmt.Println("error reading conn: ", err.Error())
 		os.Exit(1)
 	}
 
-	err = conn.Close()
-	if err != nil {
-		fmt.Println("error closing conn", err.Error())
-		os.Exit(1)
+	var statusLine []byte
+
+	for i, c := range buffer[:n] {
+		// found first CRLF
+		if c == '\r' && i+1 < n && buffer[i+1] == '\n' {
+			statusLine = buffer[:i]
+			break
+		}
 	}
 
-	fmt.Printf("written %d bytes\n", n)
+	if statusLine != nil {
+		parts := strings.Split(string(statusLine), " ")
+		if len(parts) == 3 {
+			if parts[0] == "GET" && parts[1] == "/" {
+				fmt.Println("Found", parts)
+				_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+				if err != nil {
+					fmt.Println("error writing response", err.Error())
+				}
+
+				return
+			}
+		}
+	}
+
+	fmt.Println("not found")
+	_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	if err != nil {
+		fmt.Println("error writing 404 response", err.Error())
+	}
 }
